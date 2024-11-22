9d403e229c1de9950cb1a53ecd7d3c9c05b02da1
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-11-22 13:10:22 +0600

    finally done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Db } = require('mongodb');
 
 
 require('dotenv').config()
@@ -12,8 +12,10 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
+//const uri =`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@primary.7gyeb.mongodb.net/?retryWrites=true&w=majority&appName=Primary`
 
-const uri = process.env.MDB_CONNECTION_STRING;
+console.log('this is the passwod from env file',process.env.DB_PASSWORD)
+ const uri = process.env.MDB_CONNECTION_STRING;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,13 +28,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    console.log('running the function')
     await client.connect();
     const db = client.db("FEC_Clearence");
     const studentsCollection = db.collection("students");
 
-    app.get("/students",async(req,res)=>{
-        const email = req.query.email;
-        const resp = studentsCollection.find({}).toArray();
+    app.get("/student",async(req,res)=>{
+      console.log('into the get req of students')
+        // const email = req.query.email;
+        const resp = await studentsCollection.find({}).toArray();
         res.send(resp);
     })
 
@@ -47,7 +51,14 @@ async function run() {
         res.send(isUserExist);
     })
 
+    app.get("/student",async(req,res)=>{
+      // const email = req.params.email;
+      const result = await studentsCollection.find();
+      res.send(result);
+    })
+
     app.get("/student/:email",async(req,res)=>{
+      console.log('this is from personal detail')
       const email = req.params.email;
       const result = await studentsCollection.findOne({email: email});
       res.send(result);
@@ -62,19 +73,21 @@ async function run() {
     })
 
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
+  }catch (error) {
+    console.log("Error connecting to MongoDB:", error);
   }
 }
-run().catch(console.log);
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`connected to database`)
+
 })
 
 //nothing to say
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`express app listening on port ${port}`)
 })
